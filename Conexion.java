51aeb696ec70b9f4d341af37285d3329b7f6e60e
edgvac2298 <edgardovac2298@gmail.com>
2021-12-30 12:31:36 -0500

    Upload the .java files

---
+++ Conexion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mensajes_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Edgardo
+ */
+public class Conexion {
+    public Connection get_connection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app", "root", "");
+            if (connection != null) {
+                System.out.println("Conexión exitosa");
+            }
+        } catch(SQLException e) {
+            System.out.println(e);
+        }
+        return connection;
+    }
+}
